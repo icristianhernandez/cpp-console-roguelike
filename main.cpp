57f5b2ea8c43e247b57ed0186c79a43cc13d9a00
Cristian Hernández <129994548+crishgit@users.noreply.github.com>
2023-07-19 22:06:28 -0400

    basic main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,84 @@
+#include <cstdio>
+#include <string>
+#include <vector>
 #include <iostream>
-#include <ncurses.h>
+#include <stdio.h>
+using namespace std;
 
-#include "src/game.hpp"
+string allVectorInString (vector<string> vector_of_string) {
+    string vector_in_string = "";
+
+    for(int i = 0; i < vector_of_string.size(); i++) {
+        vector_in_string += vector_of_string[i] + "\n";
+    }
+
+    return vector_in_string;
+}
+
+void insertElement (char element, int x_pos, int y_pos, vector<string>& map) {
+    map[x_pos][y_pos] = element;
+}
 
 int main() {
-  initscr();
-  curs_set(0);
+    bool game_running = true;
+    char user_key_action = ' ';
 
-  Game game;
-  game.init();
+    // samples // 
+    vector<string> map_vector = {
+        "#######",
+        "#     #",
+        "#     #",
+        "#     #",
+        "#######",
+    };
 
-  while (true) {
-    game.update();
-    game.render();
-    getch();
-  }
+    insertElement('@', 1, 1, map_vector);
+    string map_string = allVectorInString(map_vector);
 
-  endwin();
-  return 0;
+    // samples end // 
+
+    while(game_running) {
+        cout << "\033[2J\033[1:1H";
+
+        // handle user input
+        if(user_key_action == 'q') {
+            game_running = false;
+        }
+
+        // update game logic 
+        
+        // render game objects 
+        cout << map_string;
+
+        // get user input 
+        user_key_action = getchar();
+    }
+
+    return 0;
 }
+
+/* 
+static constexpr const char* SAMPLE_MAP[] = {
+    "##############################################",
+    "#######################      #################",
+    "#####################    #     ###############",
+    "######################  ###        ###########",
+    "##################      #####             ####",
+    "################       ########    ###### ####",
+    "###############      #################### ####",
+    "################    ######                  ##",
+    "########   #######  ######   #     #     #  ##",
+    "########   ######      ###                  ##",
+    "########                                    ##",
+    "####       ######      ###   #     #     #  ##",
+    "#### ###   ########## ####                  ##",
+    "#### ###   ##########   ###########=##########",
+    "#### ##################   #####          #####",
+    "#### ###             #### #####          #####",
+    "####           #     ####                #####",
+    "########       #     #### #####          #####",
+    "########       #####      ####################",
+    "##############################################",
+};
+
+*/
